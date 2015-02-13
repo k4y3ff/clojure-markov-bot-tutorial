9b1b8b5c2ea4798ffbe5f3e62e8b19e364cd397d
Katherine Fellows <k@kpf.me>
2015-02-12 22:42:26 -0800

    Add lein-gorilla dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject clojure-markov-bot-tutorial "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A tutorial for writing a basic Markov bot in Twitter."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :plugins [[lein-gorilla "0.3.4"]]
+  :main ^:skip-aot clojure-markov-bot-tutorial.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
